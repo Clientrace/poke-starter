98b0758e5d4404a26b3e31ec5f892338adead8e7
Clarence Penaflor <kimc.penaflor@gmail.com>
2023-03-20 23:35:47 +0800

    add topnav to default layout

--- client/src/layouts/generalLayout.tsx
+++ client/src/layouts/generalLayout.tsx
@@ -1,5 +1,24 @@
 
+import type { ReactElement, ReactNode } from "react"
+import TopNav from "../components/navs/topnav"
+import GeneralHeaders from '../components/headers/general'
 
+interface GeneralLayoutProps {
+  children: ReactNode
+}
 
+const GeneralLayout = (props: GeneralLayoutProps) : ReactElement => {
+  return (
+    <>
+      <TopNav/>
+      <GeneralHeaders/>
+      <div>
+        { props.children }
+      </div>
+    </>
+  )
+}
+
+export default GeneralLayout
 
 
