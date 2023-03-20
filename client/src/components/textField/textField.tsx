3cad0db3ad7da57dc4508ca6c733283e742bfbdb
Clarence Penaflor <kimc.penaflor@gmail.com>
2023-03-21 02:43:32 +0800

    implement textfield param state

--- client/src/components/textField/textField.tsx
+++ client/src/components/textField/textField.tsx
@@ -4,14 +4,24 @@ import style from './textField.module.scss';
 
 interface InputProps {
   header: string,
+  name: string,
+  value: string,
+  onChange: Function,
   placeholder: string,
 }
 
 const TextField = (props: InputProps): ReactElement => {
   return (
     <>
-      { props.header }
-      <input className={style['input']} placeholder={props.placeholder}/>
+      <div className={style['header']}>
+        { props.header }
+      </div>
+      <input
+        onChange={(e)=>props.onChange(e)}
+        name={props.name}
+        className={style['input']}
+        value={props.value}
+        placeholder={props.placeholder}/>
     </>
   )
 }
