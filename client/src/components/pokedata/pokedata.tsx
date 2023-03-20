dbc4945c24544a1b62c4dd2b2c1a6592b28db796
Clarence Penaflor <kimc.penaflor@gmail.com>
2023-03-21 04:55:13 +0800

    implement pokedata info card

---
+++ client/src/components/pokedata/pokedata.tsx
@@ -0,0 +1,67 @@
+
+import { ReactElement, ReactNode } from "react";
+import style from './pokedata.module.scss';
+
+
+interface TypeBadgeProps {
+  type: string,
+}
+
+const TypeBadge = (props: TypeBadgeProps) : ReactElement => {
+  const getTypeColor = (type: string): string => {
+    switch(type){
+      case "fire":
+        return "#B22222";
+      case "water":
+        return "#0F5E9C";
+      case "grass":
+        return "#567D46";
+      // add more types here
+    }
+    return "#666";
+  }
+  return <div className={style['type-badge']} style={{
+    backgroundColor: getTypeColor(props.type)
+  }}>
+    <b>{props.type}</b>
+  </div>
+}
+
+interface PokedataProps {
+  imageSrc: string,
+  name: string,
+  type: string,
+  flavorText: string,
+  number: string,
+  height: number,
+  weight: number,
+}
+
+const PokeData = (props: PokedataProps) : ReactElement => {
+  return <div className={style['container']}>
+    <div className={style['poke-sprite']}>
+      <img src={props.imageSrc}/>
+    </div>
+    <div className={style['info']}>
+      <b>{props.name}</b> <a className={style['poke-num']}>{props.number}</a>
+      <TypeBadge type={props.type}/>
+      <div className={style['flavor-text']}>
+        <i>{ props.flavorText }</i>
+      </div>
+      <div className={style['basic-data']}>
+        <a className={style['label']}>
+          Height: 
+        </a>
+        {props.height} <br/>
+        <a className={style['label']}>
+          Weight
+        </a>
+        {props.weight}
+      </div>
+    </div>
+  </div>
+}
+
+export default PokeData;
+
+
