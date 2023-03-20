1f7ef82fc8ea021f3e7a02b242852679a4b645bf
Clarence Penaflor <kimc.penaflor@gmail.com>
2023-03-20 23:35:30 +0800

    implement top navigation header

---
+++ client/src/components/navs/topnav.tsx
@@ -0,0 +1,16 @@
+
+import type { ReactElement } from "react"
+import style from './topnav.module.scss'
+
+const TopNav = () : ReactElement => {
+  return (
+    <div className={style['container']}>
+      Poke Starter
+    </div>
+  )
+}
+
+export default TopNav
+
+
+
