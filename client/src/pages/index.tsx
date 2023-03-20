27c185dbcd0259f39b2e13235c043460b4bd8cba
Clarence Penaflor <kimc.penaflor@gmail.com>
2023-03-21 02:42:54 +0800

    implement initial login layout

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,15 +1,50 @@
+
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import styles from './home.module.scss'
+import style from './home.module.scss';
+import GeneralLayout from '../layouts/generalLayout'
+import TextField from '../components/textField/textField';
+import Button from '../components/button/button';
+import { useState } from 'react';
+
 
 const Home: NextPage = () => {
+  const [username, setUsername] = useState("");
+
+  // Login user & create db record
+  const login = (): void => {
+    console.log(username);
+  }
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Poke Starter</title>
-        <meta name="description" content="Choose your pokemon" />
-      </Head>
-    </div>
+    <GeneralLayout>
+      <div className={style['container']}>
+        <div className={style['welcome']}>
+          <div className={style['header']}>
+            Welcome to Poké Starter!
+          </div>
+          <div className={style['subheader']}>
+            Begin your own Pokémon Adventure
+          </div>
+        </div>
+        <div className={style['form']}>
+          <div className={style['fields']}>
+            <div className={style['username']}>
+              <TextField
+                name="username"
+                value={username}
+                onChange={(e: { target: HTMLInputElement}) : void=>{
+                  setUsername(e.target.value);
+                }}
+                header="Enter a cool username to get started:"
+                placeholder="Alakazam"/>
+            </div>
+            <div className={style['action']}>
+              <Button title="Login" onClick={():void=>login()}/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </GeneralLayout>
   )
 }
 
