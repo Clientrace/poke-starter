0480f6ddd854717e2a283af395c20e40452f7c7d
Clarence Penaflor <kimc.penaflor@gmail.com>
2023-03-22 04:07:27 +0800

    implement http request validator

---
+++ server/helpr/validateRequest.js
@@ -0,0 +1,45 @@
+
+
+
+const validateRequest = (requestObj, requestFormat) => {
+
+  // validate path parameters
+  Object.keys(requestFormat.pathParams).map(key=>{
+    if(!(key in requestObj.pathParams)){
+      throw new Error('Missing required path parameter field: '+key);
+    }
+    const paramValue = requestObj.pathParams[key];
+    if(typeof(paramValue) !== requestFormat.pathParams[key]){
+      throw new Error(`Invalid field type. ${key}  must be ${requestFormat.pathParams[key]}.`);
+    }
+  });
+
+  // validate query parameters
+  Object.keys(requestFormat.queryParams).map(key=>{
+    if(!(key in requestObj.queryParams)){
+      throw new Error('Missing required query parameter field: '+key);
+    }
+    const paramValue = requestObj.queryParams[key];
+    if(typeof(paramValue) !== requestFormat.queryParams[key]){
+      throw new Error(`Invalid field type. ${key}  must be ${requestFormat.queryParams[key]}.`);
+    }
+  });
+
+  // validate request body
+  Object.keys(requestFormat.requestBody).map(key=>{
+    if(!(key in requestObj.requestBody)){
+      throw new Error('Missing required request body field: '+key);
+    }
+    const paramValue = requestObj.requestBody[key];
+    if(typeof(paramValue) !== requestFormat.requestBody[key]){
+      // throw new Error(`Invalid field type. ${key}  must be ${requestFormat.requestBody[key]}.`);
+    }
+  });
+
+}
+
+module.exports.validateRequest = validateRequest;
+
+
+
+
